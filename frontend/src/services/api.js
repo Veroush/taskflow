f21e84b4f895faf95116529204cae268faacf2a9
Veroush <ramjiawanveroushka95@gmail.com>
2026-06-27 23:53:04 -0300

    Configure CORS for GitHub Pages

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,14 +1,21 @@
 import axios from 'axios';
 
+const baseURL =
+  import.meta.env.MODE === 'development'
+    ? 'http://localhost:5000/api'
+    : 'https://taskflow-126l.onrender.com/api';
+
 const api = axios.create({
-  baseURL: '/api',
+  baseURL,
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
